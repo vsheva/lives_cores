c211b3a58faa3e17a09012e165e5ee04ba2e86d5
art-litv <artem15052003@gmail.com>
2022-10-03 15:37:33 +0300

    Update SC SSR support

--- next.config.js
+++ next.config.js
@@ -1,7 +1,18 @@
-/** @type {import('next').NextConfig} */
+const withTM = require("next-transpile-modules")([
+  "@mui/material",
+  "@mui/system",
+]); // pass the modules you would like to see transpiled
+
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      "@mui/styled-engine": "@mui/styled-engine-sc",
+    };
+    return config;
+  },
+};
 
-module.exports = nextConfig
+module.exports = withTM(nextConfig);
