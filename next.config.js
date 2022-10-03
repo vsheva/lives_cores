64c91a00bc59b123ab876e6c66a0a9d174f23b49
art-litv <artem15052003@gmail.com>
2022-10-03 16:47:05 +0300

    Fix SSR styles mismatch

--- next.config.js
+++ next.config.js
@@ -6,13 +6,6 @@ const withTM = require("next-transpile-modules")([
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  webpack: (config) => {
-    config.resolve.alias = {
-      ...config.resolve.alias,
-      "@mui/styled-engine": "@mui/styled-engine-sc",
-    };
-    return config;
-  },
   compiler: {
     styledComponents: true,
   },
