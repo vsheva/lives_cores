d597a8a290147ff9e59724caafdf9d180a3830ea
art-litv <artem15052003@gmail.com>
2022-10-04 18:16:47 +0300

    Update Filter buttons

--- src/components/Buttons/FilterButton/index.tsx
+++ src/components/Buttons/FilterButton/index.tsx
@@ -1,16 +1,20 @@
-import type { ButtonTypeMap, ExtendButtonBase } from "@mui/material";
-import React from "react";
+import { Button } from "@mui/material";
+import styled from "styled-components";
 
-import * as Styled from "./FilterButton.styled";
+const FilterButton = styled((props) => (
+  <Button variant="contained" {...props} />
+))<{ active: boolean }>`
+  font-size: 0.85rem;
+  line-height: 1.6;
+  pointer-events: ${({ active }) => active && "none"};
+  color: ${({ theme, active }) =>
+    active ? theme.palette.primary.contrastText : theme.palette.text.secondary};
+  background-color: ${({ theme, active }) =>
+    active ? theme.palette.primary.main : theme.palette.background.default};
 
-export type FilterButtonProps = {
-  active?: boolean;
-};
-
-const FilterButton: ExtendButtonBase<
-  ButtonTypeMap<FilterButtonProps, "button">
-> = ({ active = false, ...props }) => {
-  return <Styled.FilterButton active={active} {...props} />;
-};
+  &:hover {
+    background-color: ${({ theme }) => theme.palette.grey[300]};
+  }
+`;
 
 export default FilterButton;
