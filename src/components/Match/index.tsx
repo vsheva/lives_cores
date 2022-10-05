9d29bcf70fe4dd0a2a2dac48d887fa87c798e84d
art-litv <artem15052003@gmail.com>
2022-10-05 19:56:50 +0300

    Update match component layout

--- src/components/Match/index.tsx
+++ src/components/Match/index.tsx
@@ -1,6 +1,6 @@
 import type MatchType from "@entities/Match";
 import React from "react";
-import { Grid } from "@mui/material";
+import { Box, Grid, Typography } from "@mui/material";
 
 import FavoriteCheckbox from "@components/Checkboxes/FavoriteCheckbox";
 import { getRgba } from "@common/utils/palette";
@@ -12,270 +12,58 @@ type MatchProps = {
 
 const Match: React.FC<MatchProps> = ({ match }) => {
   return (
-    <Grid
-      container
+    <Box
       sx={{
-        borderBottom: `1px solid ${getRgba(
-          theme.palette.secondary.dark,
-          0.06
-        )}`,
-        padding: 0.5,
+        display: "grid",
+        alignItems: "center",
+        gridTemplate: `
+          " check       dropdown                 stageOrTime homeLogo             homeParticipant homeScore homePart1 homePart2 iconStr iconInf iconTv iconStd liveIcon" minmax(23px,auto)
+          " check       dropdown                 stageOrTime awayLogo             awayParticipant awayScore awayPart1 awayPart2 iconStr iconInf iconTv iconStd liveIcon" minmax(23px,auto) /max-content 0px 90px 24px 1fr 34px 34px 34px 28px 24px 24px 38px 31px
+        `,
+        position: "relative",
+        fontSize: "0.9rem",
+        borderBottom: `1px solid ${theme.palette.support.main}`,
+        "&:before": {
+          background: theme.palette.support.main,
+          content: '""',
+          height: "calc(100% - 12px)",
+          position: "absolute",
+          right: "144px",
+          top: "6px",
+          width: "1px",
+        },
       }}
     >
-      <Grid
-        item
-        direction="row"
-        container
-        xs={2}
-        wrap="nowrap"
-        sx={{ alignItems: "center", fontSize: "0.9rem" }}
+      <Box sx={{ gridArea: "check" }}>
+        <FavoriteCheckbox />
+      </Box>
+      <Box
+        sx={{ gridArea: "stageOrTime", color: theme.palette.text.secondary }}
       >
-        <Grid item>
-          <FavoriteCheckbox />
-        </Grid>
-        <Grid item sx={{ color: theme.palette.support.darker }}>
-          Завершено
-        </Grid>
-      </Grid>
-      <Grid
-        item
-        container
-        xs
-        direction="column"
-        sx={{ fontSize: "0.9rem", gap: 0.5 }}
-      >
-        <Grid
-          item
-          sx={{
-            display: "flex",
-            alignItems: "center",
-            gap: 1,
-          }}
-        >
-          {/* TODO: Replace with next/image when moved locally */}
-          <img
-            style={{ width: 16 }}
-            src="https://www.flashscore.ua/res/image/data/r5iH05UH-bm1huYuJ.png"
-          />
-          Зіра 2
-        </Grid>
-        <Grid
-          item
-          sx={{
-            display: "flex",
-            alignItems: "center",
-            gap: 1,
-            fontWeight: 600,
-          }}
-        >
-          <img
-            style={{ width: 16 }}
-            src="https://www.flashscore.ua/res/image/data/x6v5exT0-OvlpVrSr.png"
-          />
-          Араз
-        </Grid>
-      </Grid>
-      <Grid
-        item
-        container
-        xs={0.5}
-        direction="column"
-        sx={{ fontSize: "0.9rem", gap: 0.5 }}
-      >
-        <Grid
-          item
-          sx={{
-            display: "flex",
-            alignItems: "center",
-            fontWeight: 600,
-          }}
-        >
-          0
-        </Grid>
-        <Grid
-          item
-          sx={{
-            display: "flex",
-            alignItems: "center",
-            fontWeight: 600,
-          }}
-        >
-          1
-        </Grid>
-      </Grid>
-      <Grid
-        item
-        container
-        xs={0.5}
-        direction="column"
-        sx={{ fontSize: "0.9rem", gap: 0.5 }}
-      >
-        <Grid
-          item
-          sx={{
-            display: "flex",
-            alignItems: "center",
-            color: theme.palette.support.dark,
-          }}
-        >
-          (0)
-        </Grid>
-        <Grid
-          item
-          sx={{
-            display: "flex",
-            alignItems: "center",
-            color: theme.palette.support.dark,
-          }}
-        >
-          (1)
-        </Grid>
-      </Grid>
-      <Grid
-        item
-        container
-        xs={0.5}
-        direction="column"
-        sx={{ fontSize: "0.9rem", gap: 0.5 }}
-      >
-        <Grid
-          item
-          sx={{
-            display: "flex",
-            alignItems: "center",
-            color: theme.palette.support.dark,
-          }}
-        ></Grid>
-        <Grid
-          item
-          sx={{
-            display: "flex",
-            alignItems: "center",
-            color: theme.palette.support.dark,
-          }}
-        ></Grid>
-      </Grid>
-      <Grid
-        item
-        container
-        xs={1}
-        direction="column"
-        sx={{ fontSize: "0.9rem", gap: 0.5 }}
-      >
-        <Grid
-          item
-          sx={{
-            display: "flex",
-            alignItems: "center",
-            color: theme.palette.support.dark,
-          }}
-        ></Grid>
-        <Grid
-          item
-          sx={{
-            display: "flex",
-            alignItems: "center",
-            color: theme.palette.support.dark,
-          }}
-        ></Grid>
-      </Grid>
-      <Grid
-        item
-        container
-        xs={0.5}
-        direction="column"
-        sx={{ fontSize: "0.9rem", gap: 0.5 }}
-      >
-        <Grid
-          item
-          sx={{
-            display: "flex",
-            alignItems: "center",
-            color: theme.palette.support.dark,
-          }}
-        ></Grid>
-        <Grid
-          item
-          sx={{
-            display: "flex",
-            alignItems: "center",
-            color: theme.palette.support.dark,
-          }}
-        ></Grid>
-      </Grid>
-      <Grid
-        item
-        container
-        xs={0.5}
-        direction="column"
-        sx={{ fontSize: "0.9rem", gap: 0.5 }}
-      >
-        <Grid
-          item
-          sx={{
-            display: "flex",
-            alignItems: "center",
-            color: theme.palette.support.dark,
-          }}
-        ></Grid>
-        <Grid
-          item
-          sx={{
-            display: "flex",
-            alignItems: "center",
-            color: theme.palette.support.dark,
-          }}
-        ></Grid>
-      </Grid>
-      <Grid
-        item
-        container
-        xs={0.5}
-        direction="column"
-        sx={{ fontSize: "0.9rem", gap: 0.5 }}
-      >
-        <Grid
-          item
-          sx={{
-            display: "flex",
-            alignItems: "center",
-            color: theme.palette.support.dark,
-          }}
-        ></Grid>
-        <Grid
-          item
-          sx={{
-            display: "flex",
-            alignItems: "center",
-            color: theme.palette.support.dark,
-          }}
-        ></Grid>
-      </Grid>
-      <Grid
-        item
-        container
-        xs={0.5}
-        direction="column"
-        sx={{ fontSize: "0.9rem", gap: 0.5 }}
-      >
-        <Grid
-          item
-          sx={{
-            display: "flex",
-            alignItems: "center",
-            color: theme.palette.support.dark,
-          }}
-        ></Grid>
-        <Grid
-          item
-          sx={{
-            display: "flex",
-            alignItems: "center",
-            color: theme.palette.support.dark,
-          }}
-        ></Grid>
-      </Grid>
-    </Grid>
+        Завершено
+      </Box>
+      <img
+        style={{
+          width: 16,
+          gridArea: "homeLogo",
+        }}
+        src="https://www.flashscore.ua/res/image/data/r5iH05UH-bm1huYuJ.png"
+      />
+      <img
+        style={{ width: 16, gridArea: "awayLogo" }}
+        src="https://www.flashscore.ua/res/image/data/x6v5exT0-OvlpVrSr.png"
+      />
+      <Box sx={{ gridArea: "homeParticipant" }}>Зіра 2</Box>
+      <Box sx={{ gridArea: "awayParticipant", fontWeight: 600 }}>Араз</Box>
+      <Box sx={{ gridArea: "homeScore", fontWeight: 600 }}>0</Box>
+      <Box sx={{ gridArea: "awayScore", fontWeight: 600 }}>1</Box>
+      <Box sx={{ gridArea: "homePart1", color: theme.palette.support.dark }}>
+        (0)
+      </Box>
+      <Box sx={{ gridArea: "awayPart1", color: theme.palette.support.dark }}>
+        (1)
+      </Box>
+    </Box>
   );
 };
 
