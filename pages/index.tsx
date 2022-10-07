4f8630e04f5345274b5a3cc53e5415c08924f840
art-litv <artem15052003@gmail.com>
2022-10-07 15:34:54 +0300

    Update props

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,10 +2,13 @@ import type { NextPage } from "next";
 import { useEffect, useState } from "react";
 
 import type SportEvents from "@entities/SportEvents";
+import type SportNameId from "@entities/SportNameId";
 import * as SportEventsGateway from "@gateways/sport-events";
 import CountriesList from "@components/AsideList/Countries";
 import Livetable from "@components/Livetable";
 import Layout from "@components/Layout";
+
+const SPORT_NAME_ID: SportNameId = "football";
 
 const Home: NextPage = () => {
   const [footballSportEvents, setFootballSportEvents] = useState<SportEvents>({
@@ -14,13 +17,22 @@ const Home: NextPage = () => {
 
   useEffect(() => {
     (async () => {
-      const sportEvents = await SportEventsGateway.getSportEvents("football");
+      const sportEvents = await SportEventsGateway.getSportEvents(
+        SPORT_NAME_ID
+      );
       setFootballSportEvents(sportEvents);
     })();
   }, []);
 
   return (
-    <Layout aside={<CountriesList countries={footballSportEvents.countries} />}>
+    <Layout
+      aside={
+        <CountriesList
+          countries={footballSportEvents.countries}
+          sportNameId={SPORT_NAME_ID}
+        />
+      }
+    >
       <Livetable sportEvents={footballSportEvents.sportEvents} />
     </Layout>
   );
