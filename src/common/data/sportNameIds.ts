d7f4e5975d54a31eb91ecdf0c01afcacd52381be
art-litv <artem15052003@gmail.com>
2022-10-10 14:16:51 +0300

    Add other sport name ids to constant

--- src/common/data/sportNameIds.ts
+++ src/common/data/sportNameIds.ts
@@ -2,6 +2,11 @@ import type SportNameId from "@entities/SportNameId";
 
 const SPORT_NAME_IDS: Record<string, SportNameId> = {
   football: "football",
+  basketball: "basketball",
+  tenis: "tenis",
+  hokey: "hokey",
+  volleyball: "volleyball",
+  handball: "handball",
 };
 
 export default SPORT_NAME_IDS;
